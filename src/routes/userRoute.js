cea7f763ea723dfd6f17af914a7192cede458a47
rachard656 <richardhemba66@gmail.com>
2024-09-15 19:57:55 +0100

    joiValidationAuth

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { login, refreshToken, register } from '../controllers/userController.js'
+import { loginRequest, registerRequest } from '../validations/userRequest.js'
 
 const userRoute = express.Router()
 
-userRoute.post('/register', register)
-userRoute.post('/login', login)
+userRoute.post('/register', registerRequest, register)
+userRoute.post('/login', loginRequest, login)
 userRoute.post('/refresh/token', refreshToken)
 
 export default userRoute
--- src/validations/userRequest.js
+++ src/validations/userRequest.js
@@ -2,11 +2,63 @@ import joi from 'joi'
 
 const password_regex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!.@#$%^&*])(?=.{8,})')
 
-export const registerRequest = joi.object({
-    name: joi.string().required(),
-    email: joi.string().email().required(),
-    phone: joi.number().min(12).required(),
-    password: joi.string().pattern(password_regex).min(8).required(),
-    adress: joi.string().required(),
-    ville: joi.string().required(),
-})
+
+export const registerRequest = async (req, res, next) => {
+
+    const register = joi.object({
+        name: joi.string().required().messages({
+            'string.empty': 'Name is required',
+        }),
+        email: joi.string().email().required().messages({
+            'string.email': 'Invalid email address',
+            'string.empty': 'Email is required',
+        }),
+        phone: joi.string().min(9).required().messages({
+           'string.min': 'Invalid phone number',
+            'string.empty': 'Phone number is required',
+        }),
+        password: joi.string().pattern(password_regex).min(8).required().messages({
+            'string.min': 'Invalid password',
+            'string.pattern.base': 'Invalid password',
+            'string.empty': 'Password is required',
+        }),
+        address: joi.string().required().messages({
+            'string.empty': 'Adress is required',
+        }),
+        city: joi.string().required().messages({
+            'string.empty': 'City is required',
+        }),
+    });
+
+    const { error } = register.validate(req.body, { abortEarly: false });
+
+    if (error) {
+        return res.status(400).json({ error: error.details.map(detail => detail.message).join(', ') });
+    }
+
+    next();
+};
+
+
+export const loginRequest = async (req, res, next) => {
+    const login = joi.object({
+
+        email: joi.string().email().required().messages({
+            'string.email': 'Invalid email address or password',
+            'string.empty': 'Email is required',
+        }),
+        password: joi.string().pattern(password_regex).min(8).required().messages({
+            'string.min': 'Invalid email address or password',
+            'string.pattern.base': 'Invalid email address or password',
+            'string.empty': 'Password is required',
+        }),
+    })
+
+    const {error} = login.validate(req.body, {abortEarly:false});
+
+    if ({error}) {
+        return res.status(400).json({ error: error.details.map(detail => detail.message).join(', ') });
+    }
+
+    next()
+}
